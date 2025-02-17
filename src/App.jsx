314bac34c829392efb2eee673ef8b7d19fe95e7e
shrey <whitespacecoding@gmail.com>
2025-02-17 22:45:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Animation from './components/Animation';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Animation />
+    </>
+  );
+}
+
+export default App;
